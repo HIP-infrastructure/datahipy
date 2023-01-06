a8b27753f2c372ba528dc230f56eaac7ff968e63
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2023-01-06 17:41:57 +0100

    refactor: move validate_bids_dataset() to new ./bids/validation.py

--- bids_tools/bids/utils.py
+++ bids_tools/bids/utils.py
@@ -19,6 +19,7 @@ from bids_tools.bids.const import (
     BIDSTSVFILE_DATATYPE_KEY_MAP,
     VALID_EXTENSIONS,
 )
+from bids_tools.bids.validation import validate_bids_dataset
 
 
 NUM_THREADS = os.cpu_count() - 1 if os.cpu_count() > 1 else 1
@@ -48,43 +49,6 @@ def create_bids_layout(container_dataset_path=None, **kwargs):
         **kwargs,
     )
     return layout
-
-
-def validate_bids_dataset(container_dataset_path, *args):
-    """Validate a BIDS dataset using the BIDS Validator.
-
-    Parameters
-    ----------
-    container_dataset_path : str
-        Path to the BIDS dataset
-
-    args : list
-        List of options to pass to the `bids-validator`.
-        Can be used to ignore warnings (`["--ignoreWarnings"]`)
-        or nifti headers (`["--ignoreNiftiHeaders"]`) or to
-        skip checking that any given file for one subject
-        is present for all other subjects (`["--ignoreSubjectConsistency"]`)
-        or to use a specific BIDS schema (`["-s", "v1.6.0"]`)
-
-    Returns
-    -------
-    output : dict
-        Output of the bids-validator as a dictionary.
-
-    return_code : int
-        Return code of the bids-validator.
-    """
-    # Create the bids-validator command to run
-    command = ["bids-validator", container_dataset_path] + list(args)
-    # Add the --json option if not already present
-    if "--json" not in list(args):
-        command.append("--json")
-    # Run the command to validate the dataset
-    print(f'Execute cmd: {" ".join(command)}')
-    output = subprocess.run(command, capture_output=True)
-    print(f"Output: {output}")
-    # Return the JSON string output as a dictionary and the return code
-    return json.loads(output.stdout.decode()), output.returncode
 
 
 def get_subject_bidsfile_info(container_dataset_path, **kwargs):
