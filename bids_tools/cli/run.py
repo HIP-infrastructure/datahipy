f3f70310428123f30c580184ec1d12b0c87b59c2
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2023-01-06 17:50:00 +0100

    style: reformat with black

--- bids_tools/cli/run.py
+++ bids_tools/cli/run.py
@@ -24,10 +24,14 @@ VALID_COMMANDS = [
 def get_parser():
     """Get parser object for command line interface."""
     parser = argparse.ArgumentParser(description="BIDS dataset handler.")
-    parser.add_argument("--command", choices=VALID_COMMANDS, help="Method to be run.")
+    parser.add_argument(
+        "--command", choices=VALID_COMMANDS, help="Method to be run."
+    )
     parser.add_argument("--input_data", help="Input JSON data")
     parser.add_argument("--output_file", help="File location after processing")
-    parser.add_argument("--dataset_path", help="Path to the dataset", default="/output")
+    parser.add_argument(
+        "--dataset_path", help="Path to the dataset", default="/output"
+    )
     parser.add_argument(
         "--input_path",
         help="Path to the input data (e.g. input_data.json)",
@@ -60,7 +64,9 @@ def main():
     if command == "dataset.create":
         return dhdl.dataset_create(input_data=input_data)
     if command == "dataset.get":
-        dhdl.dataset_get_content(input_data=input_data, output_file=output_file)
+        dhdl.dataset_get_content(
+            input_data=input_data, output_file=output_file
+        )
     if command == "datasets.get":
         return get_all_datasets_content(
             input_data=input_data,
--- test/conftest.py
+++ test/conftest.py
@@ -14,17 +14,23 @@ def dataset_name():
 
 @pytest.fixture(scope="session", autouse=True)
 def dataset_path(dataset_name):
-    dataset_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "tmp", dataset_name))
+    dataset_path = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "tmp", dataset_name)
+    )
     return dataset_path
 
 
 @pytest.fixture(scope="session", autouse=True)
 def io_path():
-    io_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "tmp", "io"))
+    io_path = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "tmp", "io")
+    )
     os.makedirs(io_path, exist_ok=True)
     return io_path
 
 
 @pytest.fixture(scope="session", autouse=True)
 def input_path():
-    return os.path.abspath(os.path.join(os.path.dirname(__file__), "test_data"))
+    return os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "test_data")
+    )
