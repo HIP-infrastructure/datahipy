7d240ace15ee5fd4fe58bcd593aea723b7a918ba
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2023-02-20 17:29:25 +0100

    docs: add license and update license info

--- get_version.py
+++ get_version.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # Copyright (C) 2022, The HIP team and Contributors, All rights reserved.
-#  This software is distributed under the open-source XXX license.
+#  This software is distributed under the open-source Apache 2.0 license.
 
 """Utility script to get the version of `bids_tools`."""
 import sys
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # Copyright (C) 2022, The HIP team and Contributors, All rights reserved.
-#  This software is distributed under the open-source XXX license.
+#  This software is distributed under the open-source Apache 2.0 license.
 
 """`Setup.py` for bids_tools."""
 from os import path as op
