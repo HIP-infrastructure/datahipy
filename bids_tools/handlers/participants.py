85537d19360443f478b9e06b8d0b2079c640e653
Sébastien Tourbier <sebastien.tourbier1@gmail.com>
2023-02-22 10:24:24 +0100

    Revert "Revert "ci: implement gitlab ci workflow with semantic release""

--- bids_tools/handlers/participants.py
+++ bids_tools/handlers/participants.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2022-2023, The HIP team and Contributors, All rights reserved.
-#  This software is distributed under the open-source XXX license.
+#  This software is distributed under the open-source Apache 2.0 license.
 
 """Manage BIDS participants using BIDS Manager."""
 
--- scripts/docker/entrypoint.sh
+++ scripts/docker/entrypoint.sh
@@ -3,19 +3,23 @@
 USER=$1
 USER_ID=$2
 
-echo -n "Creating user $USER... "
-egrep "^$USER" /etc/passwd >/dev/null
-if [ $? -eq 0 ]; then
-    echo "$USER already exists and has been recreated."
-    userdel $USER
-fi
+if [ "$USER" != "root" ]; then
+    echo -n "Creating user $USER... "
+    egrep "^$USER" /etc/passwd >/dev/null
+    if [ $? -eq 0 ]; then
+        echo "$USER already exists and has been recreated."
+        userdel $USER
+    fi
 
-useradd --create-home --shell /bin/bash $USER --uid $USER_ID
-if [ $? -eq 0 ]; then
-    echo "done."
+    useradd --create-home --shell /bin/bash $USER --uid $USER_ID
+    if [ $? -eq 0 ]; then
+        echo "done."
+    else
+        echo "failed."
+        exit 1
+    fi
 else
-    echo "failed."
-    exit 1
+    echo "Running as root."
 fi
 
 runuser -u $USER -- bids_tools "${@:3}"
