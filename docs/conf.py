b7065c6abe5051acb7b659c862b88db867d7d393
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2023-07-14 18:02:03 +0200

    style: reformat '' to "" in title of docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -203,7 +203,7 @@ html_theme_options = {
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = 'DataHIPy: Data Manager of the Human Intracerebral Platform in Python'
+html_title = "DataHIPy: Data Manager of the Human Intracerebral Platform in Python"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 html_short_title = "DataHIPy"
